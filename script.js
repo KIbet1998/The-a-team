c363744684131d1425f6e6aa5702ddcb20dee1cf
ndanu-josy <josphinendanu3@gmail.com>
2021-03-17 09:20:16 +0300

    Insert images for available vacant houses

---
+++ script.js
@@ -0,0 +1,24 @@
+/*var slide = 1;
+showSlides(slide);
+
+function nextSlide(n){
+    showSlides(slide += n) ;
+}
+
+function currentSlide(n){
+    showSlides(slide = 1);
+}
+
+function displaySlides (n){
+    var i;
+    var slidesshow = document.getElementById("slides")
+    
+    if (n > slidesshow.length) {slide = 1}
+    if (n < 1){slide = slidesshow.length}
+    for (i=0; i<slidesshow.length; i++){
+        slidesshow[i].style.display = "none";
+    }
+f
+}*/
+
+
