55120741e6be9b4dd0d4e531a38d45a80e784832
ndanu-josy <josphinendanu3@gmail.com>
2021-03-17 12:55:19 +0300

    Edit script.js

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-/*var slide = 1;
-showSlides(slide);
-
-function nextSlide(n){
-    showSlides(slide += n) ;
-}
-
-function currentSlide(n){
-    showSlides(slide = 1);
-}
-
-function displaySlides (n){
-    var i;
-    var slidesshow = document.getElementById("slides")
-    
-    if (n > slidesshow.length) {slide = 1}
-    if (n < 1){slide = slidesshow.length}
-    for (i=0; i<slidesshow.length; i++){
-        slidesshow[i].style.display = "none";
-    }
-f
-}*/
 
 
